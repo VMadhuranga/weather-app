e4dbfe6db04c2fde44cf4c5acfc4260a63fc1d47
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-29 20:12:55 +0530

    Modify weather-data.js
    Create weatherData object
    Rename getWeatherData function name to fetchWeatherData
    
    Additional changes:
    - Remove today-weather.js
    - Remove tomorrow-weather.js

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,7 +1,6 @@
-import getTodayWeather from "./today-weather";
-import getTomorrowWeather from "./tommorow-weather";
+const weatherData = {};
 
-export default async function getWeatherData() {
+export default async function fetchWeatherData() {
   const API_KEY = "4be63e822db44a8d81c64432232608";
   // const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=kalutara`;
   const URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=beruwala&days=2`;
@@ -10,8 +9,6 @@ export default async function getWeatherData() {
     const response = await fetch(URL, { mode: "cors" });
     const data = await response.json();
     console.log(data);
-    getTodayWeather(data);
-    getTomorrowWeather(data);
   } catch (error) {
     console.log(error);
   }
