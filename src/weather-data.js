ad3f4b104911b344f9f3ee3529aa6629d2b1bcef
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-30 09:40:37 +0530

    Modify weather-data.js
    Create getLocation, getCurrentWeather, getTomorrowThisHourWeather
    functions
    
    Give location parameter to fetchWeatherData function
    
    Remove weatherData object, setWeatherData, setCurrentWeather,
    setTomorrowThisHourWeather

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,57 +1,74 @@
-const weatherData = {};
-
-export default async function fetchWeatherData() {
+export default async function fetchWeatherData(location = "auto:ip") {
   const API_KEY = "4be63e822db44a8d81c64432232608";
   // const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=kalutara`;
-  const URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=beruwala&days=2`;
+  const URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=2`;
 
   try {
     const response = await fetch(URL, { mode: "cors" });
     const data = await response.json();
     console.log(data);
-    setWeatherData(data);
-    setCurrentWeather(data);
-    setTomorrowThisHourWeather(data);
-    console.log(weatherData);
+    console.log(getLocation(data));
+    console.log(getCurrentWeather(data));
+    console.log(getTomorrowThisHourWeather(data));
   } catch (error) {
     console.log(error);
   }
 }
 
-function setWeatherData(data) {
-  weatherData.location = `${data.location.name} ${data.location.region} ${data.location.country}`;
-  weatherData.currentWeather = {};
-  weatherData.tomorrowThisHourWeather = {};
+function getLocation(data) {
+  const location = `${data.location.name} ${data.location.region} ${data.location.country}`;
+
+  return location;
 }
 
-function setCurrentWeather(data) {
-  weatherData.currentWeather.condition = data.current.condition.text;
-  weatherData.currentWeather.conditionIcon = data.current.condition.icon;
-  weatherData.currentWeather.humidity = data.current.humidity;
-  weatherData.currentWeather.temperatureCelsius = data.current.temp_c;
-  weatherData.currentWeather.temperatureFahrenheit = data.current.temp_f;
-  weatherData.currentWeather.windDirection = data.current.wind_dir;
-  weatherData.currentWeather.windSpeedKPH = data.current.wind_kph;
-  weatherData.currentWeather.windSpeedMPH = data.current.wind_mph;
+function getCurrentWeather(data) {
+  const dateAndTime = data.location.localtime;
+  const condition = data.current.condition.text;
+  const conditionIcon = data.current.condition.icon;
+  const humidity = data.current.humidity;
+  const temperatureCelsius = data.current.temp_c;
+  const temperatureFahrenheit = data.current.temp_f;
+  const windDirection = data.current.wind_dir;
+  const windSpeedKPH = data.current.wind_kph;
+  const windSpeedMPH = data.current.wind_mph;
+  const lastUpdated = data.current.last_updated;
+
+  return {
+    dateAndTime,
+    condition,
+    conditionIcon,
+    humidity,
+    temperatureCelsius,
+    temperatureFahrenheit,
+    windDirection,
+    windSpeedKPH,
+    windSpeedMPH,
+    lastUpdated,
+  };
 }
 
-function setTomorrowThisHourWeather(data) {
+function getTomorrowThisHourWeather(data) {
   const hour = new Date(data.location.localtime).getHours();
 
-  weatherData.tomorrowThisHourWeather.condition =
-    data.forecast.forecastday[1].hour[hour].condition.text;
-  weatherData.tomorrowThisHourWeather.conditionIcon =
-    data.forecast.forecastday[1].hour[hour].condition.icon;
-  weatherData.tomorrowThisHourWeather.humidity =
-    data.forecast.forecastday[1].hour[hour].humidity;
-  weatherData.tomorrowThisHourWeather.temperatureCelsius =
-    data.forecast.forecastday[1].hour[hour].temp_c;
-  weatherData.tomorrowThisHourWeather.temperatureFahrenheit =
-    data.forecast.forecastday[1].hour[hour].temp_f;
-  weatherData.tomorrowThisHourWeather.windDirection =
-    data.forecast.forecastday[1].hour[hour].wind_dir;
-  weatherData.tomorrowThisHourWeather.windSpeedKPH =
-    data.forecast.forecastday[1].hour[hour].wind_kph;
-  weatherData.tomorrowThisHourWeather.windSpeedMPH =
-    data.forecast.forecastday[1].hour[hour].wind_mph;
+  const dateAndTime = data.forecast.forecastday[1].hour[hour].time;
+  const condition = data.forecast.forecastday[1].hour[hour].condition.text;
+  const conditionIcon = data.forecast.forecastday[1].hour[hour].condition.icon;
+  const humidity = data.forecast.forecastday[1].hour[hour].humidity;
+  const temperatureCelsius = data.forecast.forecastday[1].hour[hour].temp_c;
+  const temperatureFahrenheit = data.forecast.forecastday[1].hour[hour].temp_f;
+  const windDirection = data.forecast.forecastday[1].hour[hour].wind_dir;
+  const windSpeedKPH = data.forecast.forecastday[1].hour[hour].wind_kph;
+  const windSpeedMPH = data.forecast.forecastday[1].hour[hour].wind_mph;
+
+  return {
+    dateAndTime,
+    condition,
+    conditionIcon,
+    humidity,
+    temperatureCelsius,
+    temperatureFahrenheit,
+    windDirection,
+    windSpeedKPH,
+    windSpeedMPH,
+  };
 }
